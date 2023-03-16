13a076006f489259431f243208aac975418a3306
Pratik Chatterjee <pratchatterjee@deloitte.com>
2023-03-16 17:25:53 +0530

    exception handling and logging

---
+++ src/exception.py
@@ -0,0 +1,27 @@
+# check for exception documents to know more.
+# we will write our own custom expection
+
+# any exception that are getting controlled the sys library will handle this.
+# check what sys module does
+
+import sys 
+from src.logger import logging
+
+
+def error_message_detail(error,error_detail:sys):
+    _,_,exc_tb=error_detail.exc_info()
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+     file_name,exc_tb.tb_lineno,str(error))
+
+    return error_message
+
+    
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+    
+    def __str__(self):
+        return self.error_message
